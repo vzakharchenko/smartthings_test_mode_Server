287ccb4110202751f9ce6a1e00bd63179e57d366
vzakharchenko <vzakharchenko@provectus.com>
2019-09-12 15:19:15 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var https = require('https');
+//var http = require('http');
+var fs = require('fs');
+
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('./private.key'),
+  cert: fs.readFileSync('./cert.cer')
+};
+
+// Create a service (the app object is just a callback).
+var app = express();
+
+app.get('/prod/lockcode', (req, res) => {
+  // const status = { status: 'OK' };
+  console.log("request "+req.originalUrl);
+  console.log("request headers: "+JSON.stringify(req.headers));
+  console.log("request params: "+JSON.stringify(req.params));
+  res.send("success");
+});
+
+// Create an HTTP service.
+// http.createServer(app).listen(80);
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(844);
